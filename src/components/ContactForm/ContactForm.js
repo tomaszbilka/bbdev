397e85f5bc48b3fd8cfedcf6fe75a9fe1395c303
tomaszbilka <bila4@op.pl>
2022-06-11 08:32:42 +0200

    bug fix: add name to form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -25,7 +25,12 @@ const ContactForm = () => {
         <p className="contact__mail">bbdevpl@gmail.com</p>
         <p className="contact__text">or send message by form below</p>
       </div>
-      <form className="form" onSubmit={formik.handleSubmit} data-netlify="true">
+      <form
+        className="form"
+        onSubmit={formik.handleSubmit}
+        netlify
+        name="contact"
+      >
         <div className="form__field">
           <label className="form__label" htmlFor="name">
             Name
